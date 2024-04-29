274a6f9e75e62a472f060157fa79e5df63517d78
AndrewBlanchard <blanchard0andrew@gmail.com>
2024-04-29 18:29:04 -0500

    Created reactionRoute.js code

---
+++ routes/reactionRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { Thought, Reaction } = require('../models');
+
+// POST to create a reaction
+router.post('/:thoughtId/reactions', async (req, res) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body } },
+      { new: true, runValidators: true }
+    );
+    if (!thought) {
+      return res.status(404).json({ message: 'No thought found with this id!' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE to remove a reaction
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+  try {
+    const thought = await Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: { reactionId: req.params.reactionId } } },
+      { new: true }
+    );
+    if (!thought) {
+      return res.status(404).json({ message: 'No thought found with this id!' });
+    }
+    res.json(thought);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
